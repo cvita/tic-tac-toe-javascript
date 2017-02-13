3c10d38f6cabd8596de835fdbd720f7db1b8ec39
Chris Vita <chris.vita@gmail.com>
2017-02-12 18:31:46 -0800

    Significant refactoring

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 "use strict";
 
-$(document).ready(function () {
-  setTimeout(makeFirstMove, 350);
-});
-
 // Todo: Create UI for player to decide if they want to go first or second, and choose their marker
 // Todo: Create score keeping feature
 // Todo: Create option for two computer opponenets, and option for two human opponents
@@ -11,163 +7,177 @@ $(document).ready(function () {
 var player1 = {
   "player": "computer",
   "marker": "x",
-  "moves": []
+  "moves": [],
+  "score": 0
 };
 
 var player2 = {
   "player": "computer",
   "marker": "o",
-  "moves": []
+  "moves": [],
+  "score": 0
 };
 
-var winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+var gameboard = {
+  "availablePositions": [0, 1, 2, 3, 4, 5, 6, 7, 8],
+  "winningCombos": [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+}
 
-$(".resetGame").click(function () {
-  $("td").html("");
-  player1.moves = [];
-  player2.moves = [];
-  setTimeout(makeFirstMove, 500);
+var numberOfGames = 0;
+
+$(document).ready(function () {
+  cycleActivePlayer(player2); // Creates condition to cue Player 1
 });
 
-function makeFirstMove() {
-  if (player1.player === "computer") {
-    var locationOfMove = Math.floor(Math.random() * 9);
-    player1.moves.push(locationOfMove);
-    $("#" + locationOfMove).html(player1.marker);
-    cycleActivePlayer(player1);
+$(".playAgain").click(function () {
+  player1.moves = [];
+  player2.moves = [];
+  gameboard.availablePositions = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  $("td").html("");
+  cycleActivePlayer(player2);
+});
+
+function playAgain() {
+  if (numberOfGames < 100) {
+    player1.moves = [];
+    player2.moves = [];
+    gameboard.availablePositions = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    $("td").html("");
+    cycleActivePlayer(player2); // Creates condition to cue Player 1
   } else {
-    makePlayerMove(player1);
-    $(".messageToPlayer").html("Player 1, go!");
+    console.log(player1.score + " to " + player2.score + ", out of " + numberOfGames + " games");
+  }
+}
+
+function scanForWinningCombo(playerNumber) {
+  for (var i = 0; i < gameboard.winningCombos.length; i++) {
+    var aWinningCombination = gameboard.winningCombos[i].every(function (val) {
+      return playerNumber.moves.indexOf(val) !== -1;
+    });
+
+    if (aWinningCombination) {
+      numberOfGames++;
+      playerNumber.score++;
+      var losingPlayer = playerNumber === player1 ? player2 : player1;
+      $(".messageToPlayer").html(playerNumber.score + " / " + losingPlayer.score + "<br>");
+      $(".messageToPlayer").append(playerNumber.player + " is the winner!<br>Game no. " + numberOfGames);
+      $(".messageToPlayer").css("transform", "scale(1.5)");
+      playAgain();
+      break;
+    }
+
+    if (i === gameboard.winningCombos.length - 1) {
+      if (player1.moves.length + player2.moves.length === 9) {
+        numberOfGames++;
+        $(".messageToPlayer").html("It's a tie!<br>Game no. " + numberOfGames);
+        $(".messageToPlayer").css("transform", "scale(1.5)");
+        playAgain();
+      } else {
+        cycleActivePlayer(playerNumber);
+      }
+    }
   }
 }
 
 function cycleActivePlayer(lastPlayerToGo) {
-  if (scanForWinningCombo(player1.moves)) {
-    $(".messageToPlayer").html("Player 1 is the winner!");
-  } else if (scanForWinningCombo(player2.moves)) {
-    $(".messageToPlayer").html("Player 2 is the winner!");
-  } else if (player1.moves.length + player2.moves.length === 9) {
-    $(".messageToPlayer").html("It's a tie!");
-    $(".messageToPlayer").css("transform", "scale(2)");
-  } else {
-    var whoseNext = lastPlayerToGo === player1 ? "Player 2" : "Player 1";
-    $(".messageToPlayer").html(whoseNext + " , go!");
-    if (lastPlayerToGo === player1) {
-      setTimeout(function () {
-        makePlayerMove(player2);
-      }, 25);
+  if (lastPlayerToGo === player1) {
+    $(".messageToPlayer").html("Player 2, go!");
+    if (player2.player === "computer") {
+      makeComputerMove(player2);
     } else {
-      setTimeout(function () {
-        makePlayerMove(player1);
-      }, 25);
+      enableHumanMove(player2);
     }
-  }
-}
-
-function scanForWinningCombo(arrayOfCurrentMoves) {
-  var winner = false;
-  winningCombos.forEach(function (combo) {
-    var winningCombination = combo.every(function (val) {
-      return arrayOfCurrentMoves.indexOf(val) !== -1;
-    });
-    if (winningCombination) {
-      winner = true;
-      $(".messageToPlayer").css("transform", "scale(2)");
-    }
-  });
-  return winner;
-}
-
-function makePlayerMove(playerNumber) {
-  if (playerNumber.player === "human") {
-    makeHumanMove(playerNumber);
   } else {
-    makeComputerMove(playerNumber);
+    $(".messageToPlayer").html("Player 1, go!");
+    if (player1.player === "computer") {
+      makeComputerMove(player1);
+    } else {
+      enableHumanMove(player1);
+    }
   }
 }
 
-function makeHumanMove(playerNumber) {
+function enableHumanMove(playerNumber) {
   $("td").click(function () {
     if ($(this).html() === "") {
       var locationOfMove = parseInt($(this).attr("id"));
+      gameboard.availablePositions[locationOfMove] = null;
       playerNumber.moves.push(locationOfMove);
       $(this).html(playerNumber.marker);
-      cycleActivePlayer(playerNumber);
+      scanForWinningCombo(playerNumber);
     }
   });
 }
 
 function makeComputerMove(playerNumber) {
-  if (playerNumber === player1) {
-    var locationOfMove = determineOffensiveMove(playerNumber);
-    console.log("case 1");
-  }
+  if (player1.moves.length === 0) {
+    var locationOfMove = Math.floor(Math.random() * 9);
+  } else {
+    if (playerNumber === player1) {
+      locationOfMove = determineOffensiveMove(playerNumber);
+    }
 
-  if (locationOfMove === undefined) {
-    locationOfMove = determineDefensiveMove(playerNumber);
-    console.log("case 2");
+    if (locationOfMove === undefined) { // !locationOfMove == 0 bug
+      locationOfMove = determineDefensiveMove(playerNumber);
+    }
+
+    if (locationOfMove === undefined) {
+      locationOfMove = selectTheFirstEmptySquare();
+    }
   }
-  if (locationOfMove === undefined) {
-    locationOfMove = selectTheFirstEmptySquare();
-    console.log("case 3");
-  }
+  gameboard.availablePositions[locationOfMove] = null;
   playerNumber.moves.push(locationOfMove);
   $("#" + locationOfMove).html(playerNumber.marker);
-  cycleActivePlayer(playerNumber);
+  scanForWinningCombo(playerNumber);
 }
 
 function determineOffensiveMove(currentPlayer) {
   var otherPlayer = currentPlayer === player1 ? player2 : player1;
-  var potentialMoves = [];
-  winningCombos.forEach(function (combo) {
+  var locationOfMove;
+  gameboard.winningCombos.forEach(function (combo) {
+    // All of players current moves are included in this combo
     var validCombination = currentPlayer.moves.every(function (val) {
       return combo.indexOf(val) !== -1;
     });
-
+    // None of the other players moves are included in this combo
     var invalidatedByOtherPlayerMoves = otherPlayer.moves.some(function (val) {
       return combo.indexOf(val) !== -1;
     });
 
     if (validCombination && !invalidatedByOtherPlayerMoves) {
-      combo.forEach(function (val) {
-        if (currentPlayer.moves.indexOf(val) === -1) {
-          potentialMoves.push(val);
-        }
+      locationOfMove = combo.find(function (val) {
+        return currentPlayer.moves.indexOf(val) === -1;
       });
     }
   });
-  return potentialMoves[Math.floor(Math.random() * potentialMoves.length)];
+  return locationOfMove;
 }
 
 function determineDefensiveMove(currentPlayer) {
   var otherPlayer = currentPlayer === player1 ? player2 : player1;
-  var locationOfMove;
-  winningCombos.forEach(function (combo) {
-    var defensiveMove = combo.filter(function (val) {
+  for (var i = 0; i < gameboard.winningCombos.length; i++) {
+    var defensiveMoves = gameboard.winningCombos[i].filter(function (val) {
       return otherPlayer.moves.indexOf(val) === -1;
     });
-    if (defensiveMove.length === 1 && currentPlayer.moves.indexOf(defensiveMove[0]) === -1) {
-      locationOfMove = defensiveMove[0];
+    if (defensiveMoves.length === 1 && gameboard.availablePositions.indexOf(defensiveMoves[0]) !== -1) {
+      return defensiveMoves[0];
     }
-  });
-  return locationOfMove;
+  }
 }
 
 function selectTheFirstEmptySquare() {
-  var locationOfMove;
-  $("td").each(function (td) {
-    if ($(this).html() === "") {
-      locationOfMove = parseInt($(this).attr("id"));
+  for (var i = 0; i < gameboard.availablePositions.length; i++) {
+    if (gameboard.availablePositions[i] !== null) {
+      return gameboard.availablePositions[i];
     }
-  });
-  return locationOfMove;
+  }
 }
